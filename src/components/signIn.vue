6e6c5a510070efd1f5afb90b741cd7002b346393
attraversiamo249 <hichemdahi57@gmail.com>
2020-10-23 11:55:03 +0100

    small changes

--- src/components/signIn.vue
+++ src/components/signIn.vue
@@ -3,39 +3,41 @@
     <v-card>
       <v-card-text>
         <validation-observer v-slot="{ invalid }">
-          <validation-provider rules="email" name="Email" v-slot="{ errors }">
-            <v-text-field
-              v-model="email"
-              class="inputs"
-              label="Email"
-              type="text"
-            />
-            <span> {{ errors[0] }}</span>
-          </validation-provider>
-          <validation-provider
-            rules="alphaNum|confirmed:confirm"
-            name="Password"
-            v-slot="{ errors }"
-          >
-            <v-text-field
-              v-model="password"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              class="inputs"
-              label="Password"
-              :type="show1 ? 'text' : 'password'"
-              @click:append="show1 = !show1"
-            />
-            <span> {{ errors[0] }}</span>
-          </validation-provider>
+          <form @submit.prevent="submit">
+            <validation-provider rules="email" name="Email" v-slot="{ errors }">
+              <v-text-field
+                v-model="email"
+                class="inputs"
+                label="Email"
+                type="text"
+              />
+              <span> {{ errors[0] }}</span>
+            </validation-provider>
+            <validation-provider
+              rules="alphaNum|confirmed:confirm"
+              name="Password"
+              v-slot="{ errors }"
+            >
+              <v-text-field
+                v-model="password"
+                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                class="inputs"
+                label="Password"
+                :type="show1 ? 'text' : 'password'"
+                @click:append="show1 = !show1"
+              />
+              <span> {{ errors[0] }}</span>
+            </validation-provider>
 
-          <v-btn
-            class="btn"
-            :disabled="invalid"
-            raised
-            color="#CFD8DC"
-            @click="Submit"
-            >Submit</v-btn
-          >
+            <v-btn
+              class="btn"
+              :disabled="invalid"
+              raised
+              color="#CFD8DC"
+              @click="Submit"
+              >Submit</v-btn
+            >
+          </form>
         </validation-observer>
       </v-card-text>
     </v-card>
--- src/components/signup.vue
+++ src/components/signup.vue
@@ -3,107 +3,109 @@
     <v-card>
       <v-card-text>
         <validation-observer v-slot="{ invalid }">
-          <validation-provider
-            v-slot="{ errors }"
-            name="First name"
-            rules="required|alpha"
-          >
-            <v-text-field
-              v-model="fname"
-              class="inputs"
-              label="First name"
-              type="text"
-            />
-            <span> {{ errors[0] }}</span>
-          </validation-provider>
-
-          <validation-provider
-            v-slot="{ errors }"
-            name="Last name"
-            rules="required|alpha"
-          >
-            <v-text-field
-              v-model="lname"
-              class="inputs"
-              label="Last name"
-              type="text"
-            />
-            <span> {{ errors[0] }}</span>
-          </validation-provider>
-
-          <validation-provider
-            v-slot="{ errors }"
-            name="Username"
-            rules="required|alphaNum"
-          >
-            <v-text-field
-              v-model="username"
-              class="inputs"
-              label="Username"
-              type="text"
-            />
-            <span> {{ errors[0] }}</span>
-          </validation-provider>
-          <validation-provider rules="email" name="Email" v-slot="{ errors }">
-            <v-text-field
-              v-model="email"
-              class="inputs"
-              label="Email"
-              type="text"
-            />
-            <span> {{ errors[0] }}</span>
-          </validation-provider>
-          <validation-observer>
+          <form @submit.prevent="submit">
             <validation-provider
-              rules="alphaNum|confirmed:confirm"
-              name="Password"
+              v-slot="{ errors }"
+              name="First name"
+              rules="required|alpha"
+            >
+              <v-text-field
+                v-model="fname"
+                class="inputs"
+                label="First name"
+                type="text"
+              />
+              <span> {{ errors[0] }}</span>
+            </validation-provider>
+
+            <validation-provider
+              v-slot="{ errors }"
+              name="Last name"
+              rules="required|alpha"
+            >
+              <v-text-field
+                v-model="lname"
+                class="inputs"
+                label="Last name"
+                type="text"
+              />
+              <span> {{ errors[0] }}</span>
+            </validation-provider>
+
+            <validation-provider
+              v-slot="{ errors }"
+              name="Username"
+              rules="required|alphaNum"
+            >
+              <v-text-field
+                v-model="username"
+                class="inputs"
+                label="Username"
+                type="text"
+              />
+              <span> {{ errors[0] }}</span>
+            </validation-provider>
+            <validation-provider rules="email" name="Email" v-slot="{ errors }">
+              <v-text-field
+                v-model="email"
+                class="inputs"
+                label="Email"
+                type="text"
+              />
+              <span> {{ errors[0] }}</span>
+            </validation-provider>
+            <validation-observer>
+              <validation-provider
+                rules="alphaNum|confirmed:confirm"
+                name="Password"
+                v-slot="{ errors }"
+              >
+                <v-text-field
+                  v-model="password"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  class="inputs"
+                  label="Password"
+                  :type="show1 ? 'text' : 'password'"
+                  @click:append="show1 = !show1"
+                />
+                <span> {{ errors[0] }}</span>
+              </validation-provider>
+              <validation-provider vid="confirm" v-slot="{ errors }">
+                <v-text-field
+                  v-model="passwordconf"
+                  :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                  class="inputs"
+                  label="Confirm password"
+                  :type="show2 ? 'text' : 'password'"
+                  @click:append="show2 = !show2"
+                />
+
+                <span> {{ errors[0] }}</span>
+              </validation-provider>
+            </validation-observer>
+
+            <validation-provider
+              rules="numeric"
+              name="Phone number"
               v-slot="{ errors }"
             >
               <v-text-field
-                v-model="password"
-                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                v-model="phoneNum"
                 class="inputs"
-                label="Password"
-                :type="show1 ? 'text' : 'password'"
-                @click:append="show1 = !show1"
+                label="Phone N°"
+                type="text"
               />
               <span> {{ errors[0] }}</span>
             </validation-provider>
-            <validation-provider vid="confirm" v-slot="{ errors }">
-              <v-text-field
-                v-model="passwordconf"
-                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-                class="inputs"
-                label="Confirm password"
-                :type="show2 ? 'text' : 'password'"
-                @click:append="show2 = !show2"
-              />
-
-              <span> {{ errors[0] }}</span>
-            </validation-provider>
-          </validation-observer>
-
-          <validation-provider
-            rules="numeric"
-            name="Phone number"
-            v-slot="{ errors }"
-          >
-            <v-text-field
-              v-model="phoneNum"
-              class="inputs"
-              label="Phone N°"
-              type="text"
-            />
-            <span> {{ errors[0] }}</span>
-          </validation-provider>
-          <v-btn
-            class="btn"
-            :disabled="invalid"
-            raised
-            color="#CFD8DC"
-            @click="Submit"
-            >Submit</v-btn
-          >
+            <v-btn
+              class="btn"
+              :disabled="invalid"
+              raised
+              color="#CFD8DC"
+              @click="Submit"
+              >Submit</v-btn
+            >
+          </form>
         </validation-observer>
       </v-card-text>
     </v-card>
