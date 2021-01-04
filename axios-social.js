176ef6a6a804d46ba7d62b28df9a09f41f6a0ddb
attraversiamo249 <hichemdahi57@gmail.com>
2021-01-04 19:12:20 +0100

    social intgeration

--- axios-social.js
+++ axios-social.js
@@ -1,19 +1,3 @@
 import Axios from "axios";
 
 Axios.defaults.baseURL = "https://kerya.herokuapp.com/auth/";
-
-// eslint-disable-next-line no-unused-vars
-function facebookLogin(authRes) {
-  Axios.post("convert-token/", {
-    token: authRes.accessToken,
-    backend: "facebook",
-    grant_type: "convert_token",
-    client_id: "FzgqAO8BN0gosZ8ACC3CIEhj0thrVkHxIjRuh37t",
-    client_secret:
-      "0PTnZIeDU2dPK11fzfGwyZXa3sYY471TZWaGOmlq6Su5lcO50OjB0hnMX3VYhtvqUyQCCZnJ2Lr65LxS1vfr7CQKwQdaFiKkS2391wqotIMxrU4ePANfwK4tojWznsrw"
-  }).then(res => {
-    console.log(res);
-    localStorage.setItem("accessToken", res.data.access_token);
-    localStorage.setItem("refreshToken", res.data.refresh_token);
-  });
-}
--- src/App.vue
+++ src/App.vue
@@ -212,8 +212,20 @@ export default {
     },
     getFbData(res) {
       console.log(res.authResponse);
-      this.fbInfo = res.authResponse;
-      axiosSocial.facebookLogin(res.authResponse);
+      axiosSocial
+        .post("convert-token/", {
+          token: res.authResponse.accessToken,
+          backend: "facebook",
+          grant_type: "convert_token",
+          client_id: "FzgqAO8BN0gosZ8ACC3CIEhj0thrVkHxIjRuh37t",
+          client_secret:
+            "0PTnZIeDU2dPK11fzfGwyZXa3sYY471TZWaGOmlq6Su5lcO50OjB0hnMX3VYhtvqUyQCCZnJ2Lr65LxS1vfr7CQKwQdaFiKkS2391wqotIMxrU4ePANfwK4tojWznsrw"
+        })
+        .then(res => {
+          console.log(res);
+          localStorage.setItem("accessToken", res.data.access_token);
+          localStorage.setItem("refreshToken", res.data.refresh_token);
+        });
     }
   },
   watch: {
