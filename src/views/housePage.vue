33da8478cfa754432d7da7ae4e062aa43dc835d8
attraversiamo249 <hichemdahi57@gmail.com>
2021-01-22 11:08:17 +0100

    social intgeration

--- src/views/housePage.vue
+++ src/views/housePage.vue
@@ -104,7 +104,7 @@
 
               <div class="d-flex mb-2">
                 <v-icon class="mr-2">mdi-facebook-messenger</v-icon>
-                <a href="http://m.me/kader.da.359/">Use messenger</a>
+                <a href="http://m.me/hichem.talos.7/">Use messenger</a>
               </div>
             </v-sheet>
           </v-col>
